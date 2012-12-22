3ec0189e8d26e54a8cec08d744c6eac341d4e585
hayashi <hayashi@qoncept.jp>
2012-12-22 14:46:58 +0900

    Initial Commit

---
+++ iOSVirtualCameraSA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iOSVirtualCameraSA
+//
+//  Created by hayashi on 12/22/12.
+//  Copyright (c) 2012 hayashi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
